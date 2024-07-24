f378774ce65ca5659b0a068470beb83aac870704
hmJeon <hmjeon@aiblab.co.kr>
2024-07-24 14:35:22 +0900

    데이터 패칭하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,31 @@
 import { useState } from 'react';
-
 import Map from '@/components/Map';
 import Markers from '@/components/Markers';
 import StoreBox from '@/components/StoreBox';
+import { StoreType } from '@/interface';
 
-import * as stores from '@/data/store_data.json';
-
-export default function Home() {
+export default function Home({ stores }: { stores: StoreType[] }) {
   const [map, setMap] = useState(null);
   const [currentStore, setCurrentStore] = useState(null);
-  const storeDatas = stores['DATA'];
 
   console.log(currentStore);
+
   return (
     <>
       <Map setMap={setMap} />
-      <Markers
-        map={map}
-        storeDatas={storeDatas}
-        setCurrentStore={setCurrentStore}
-      />
+      <Markers map={map} stores={stores} setCurrentStore={setCurrentStore} />
       <StoreBox store={currentStore} setStore={setCurrentStore} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const stores = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/stores`
+  ).then((res) => res.json());
+
+  return {
+    props: { stores },
+    revalidate: 60 * 60,
+  };
 }
